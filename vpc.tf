6fc95916c5d5b1f27e09441297826b10628d747d
mkr0205 <mkr0205@outlook.com>
2024-10-02 05:19:11 +0200

    terraform vpc module

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
-  #source                = "../terraform-aws-vpc"
-  source = "git::https://github.com/techidevops/terraform-aws-vpc.git?ref=Main"
+  source                = "../terraform-aws-vpc"
+  #source = "git::https://github.com/techidevops/terraform-aws-vpc.git?ref=main"
   project_name          = var.project_name
   environment           = var.environment
   common_tags           = var.common_tags
